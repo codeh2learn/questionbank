9ba4885cfb5225acd43bb5b08230411b0b6f8b19
CodeH <codeh2learn@gmail.com>
2020-08-04 12:19:45 +0530

    Initial commit

---
+++ src/main/java/com/codeh/questionbank/QuestionBankApplication.java
@@ -0,0 +1,13 @@
+package com.codeh.questionbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionBankApplication.class, args);
+	}
+
+}
